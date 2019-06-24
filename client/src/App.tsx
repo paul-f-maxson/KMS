9ebf22ebf284082d6b792a82d34a213ff8991a9a
Paul Maxson <paul.f.maxson@gmail.com>
2019-06-24 10:37:36 -0400

    Basic Presentational component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Presentational from './Presentational';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const orders = [
+    { table: 100, meals: [{ seat: 1, dish: 'apples' }] },
+    {
+      table: 200,
+      meals: [{ seat: 1, dish: 'apples' }, { seat: 3, dish: 'oranges' }],
+    },
+    {
+      table: 710,
+      meals: [{ seat: 2, dish: 'grapes' }, { seat: 4, dish: 'bananas' }],
+    },
+  ];
+
+  return <Presentational orders={orders} />;
+};
 
 export default App;
---
+++ client/src/Presentational.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export interface PresentationalProps {
+  orders: Array<{
+    table: number;
+    meals: Array<{ seat: number; dish: string }>;
+  }>;
+}
+
+export default ({ orders }: PresentationalProps) => (
+  <>
+    {orders.map(order => (
+      <>
+        <h2>{order.table}</h2>
+        <ul>
+          {order.meals.map(meal => (
+            <li>
+              <h3>{meal.seat}</h3>
+              <p>{meal.dish}</p>
+            </li>
+          ))}
+        </ul>
+      </>
+    ))}
+  </>
+);
